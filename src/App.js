eb98ab865a3f4ee6fa76aa6ecc39baeb08c8c171
Wellisson Lima <wellissonboanerges@gmail.com>
2024-09-12 19:49:23 -0300

    Retirada dos cards e edição do compomente Button usnado prop default

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
-//import Button from './components/button/button.js'
-import Card from './components/card/card.js'
+import Button from './components/button/button.js'
+// import Card from './components/card/card.js'
 
 function App() {
   return (
     <>
-      <Card title='Título card 1'/>
-      <Card title='Título card 2'/>
-      <Card title='Título card 3'/>
+
+      <Button lable='BuscarCV' />
+
+      <Button />
+
+      {/* <Card>
+        <h3>Título card 1</h3>
+        <p>Esse é um texto do card 1 😁</p>
+      </Card>
+
+      <Card>
+        <h3>Título card 2</h3>
+        <p>Esse é um texto do card 2 😁</p>
+      </Card>
+
+      <Card>
+        <h3>Título card 3</h3>
+        <p>Esse é um texto do card 3 😁</p>
+      </Card> */}
     </>
   );
 }
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,18 @@
+import React, {Component} from 'react'
 import './button.css'
 
-const Button = () => {
-    return <button className='btn'>Clique aqui</button>
+// const Button = (props) => {
+//     return <button className='btn'>{props.lable}</button>
+// }
+
+class Button extends Component {
+    render () {
+        return <button className='btn'>{this.props.lable}</button>
+    }
+}
+
+Button.defaultProps = {
+    lable: 'Clique aqui com classe'
 }
 
 export default Button
